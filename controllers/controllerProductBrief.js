59a2c1a79b229c6d90a4f8ec43db9de78a07186d
GunardiCahyadi <cahyadigunardi@gmail.com>
2024-08-15 15:38:08 +0700

    daily commit

--- controllers/controllerProductBrief.js
+++ controllers/controllerProductBrief.js
@@ -610,14 +610,14 @@ class ControllerProductBrief {
       if (findProductBrief?.rdSelection === "RD1") {
         const info = await transporter.sendMail({
           from: `[Notifikasi][Product Brief] - ${findProductBrief?.dataValues?.nama} <no_reply_it@lapilabs.co.id>`,
-          // to: ["gunardi.cahyadi@lapilabs.co.id"], //
-          to: [
-            "RD1.Global@lapilabs.co.id",
-            "RD3.Global@lapilabs.co.id",
-            "AD.Global@lapilabs.co.id",
-          ], // list of receivers
+          to: ["gunardi.cahyadi@lapilabs.co.id"], //
+          // to: [
+          //   "RD1.Global@lapilabs.co.id",
+          //   "RD3.Global@lapilabs.co.id",
+          //   "AD.Global@lapilabs.co.id",
+          // ], // list of receivers
           subject: "Product Brief", // Subject line
-          text: "Hello world?", // plain text body
+          text: "Product Brief", // plain text body
           html: `
           <html>
           <head>
@@ -698,14 +698,14 @@ class ControllerProductBrief {
       } else {
         const info = await transporter.sendMail({
           from: `[Notifikasi][Product Brief] - ${findProductBrief?.dataValues?.nama} <no_reply_it@lapilabs.co.id>`,
-          // to: ["gunardi.cahyadi@lapilabs.co.id"], //
-          to: [
-            "RD2.Global@lapilabs.co.id",
-            "RD3.Global@lapilabs.co.id",
-            "AD.Global@lapilabs.co.id",
-          ],
+          to: ["gunardi.cahyadi@lapilabs.co.id"], //
+          // to: [
+          //   "RD2.Global@lapilabs.co.id",
+          //   "RD3.Global@lapilabs.co.id",
+          //   "AD.Global@lapilabs.co.id",
+          // ],
           subject: "Product Brief", // Subject line
-          text: "Hello world?", // plain text body
+          text: "Product Brief", // plain text body
           html: `
           <html>
           <head>
