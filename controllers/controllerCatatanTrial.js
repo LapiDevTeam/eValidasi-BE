c3e0d46d92adba37f111c7f4206cd44153548adf
GunardiCahyadi <cahyadigunardi@gmail.com>
2024-09-09 11:18:40 +0700

    daily commit

--- controllers/controllerCatatanTrial.js
+++ controllers/controllerCatatanTrial.js
@@ -118,18 +118,47 @@ class ControllerCatatanTrial {
   }
   static async findAllNamaProduct02(req, res) {
     try {
-      const kodeTrialObatJadi = await m_kodeTrialObatJadi.findAll();
+      const config = {
+        user: process.env.MS_SQL_DB_USER,
+        password: process.env.MS_SQL_DB_PWD,
+        server: process.env.MS_SQL_DB_SERVER,
+        database: process.env.MS_SQL_DB_NAME,
+        options: {
+          encrypt: false,
+          trustServerCertificate: true,
+        },
+      };
 
-      res.status(200).json(kodeTrialObatJadi);
-    } catch (err) {
-      console.error("Error fetching data:", err);
-      res.status(500).json({
-        success: false,
-        message: "Failed to fetch data",
-        error: err.message,
+      sql.connect(config, function (err) {
+        if (err) console.log(err);
+        const request = new sql.Request();
+        request.query(
+          `SELECT Product_ID, Product_Name, Product_Category FROM m_product WHERE Product_Category = '02' AND isActive = '1';
+          `,
+          async function (err, { recordset }) {
+            if (err) console.log(err);
+            res.status(200).json(recordset);
+          }
+        );
       });
+    } catch (err) {
+      console.log(err);
     }
   }
+  // static async findAllNamaProduct02(req, res) {
+  //   try {
+  //     const kodeTrialObatJadi = await m_kodeTrialObatJadi.findAll();
+
+  //     res.status(200).json(kodeTrialObatJadi);
+  //   } catch (err) {
+  //     console.error("Error fetching data:", err);
+  //     res.status(500).json({
+  //       success: false,
+  //       message: "Failed to fetch data",
+  //       error: err.message,
+  //     });
+  //   }
+  // }
 
   static async createCatatanTrial(req, res, next) {
     try {
