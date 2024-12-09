5838f68eb4f9847d60f626bf893e1adf837a5161
Michael Fransisco <michaelsinx.one@gmail.com>
2024-12-09 15:44:14 +0700

    new feat master-bahan-awal-template

--- routers/master-vb/index.js
+++ routers/master-vb/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const { authentication } = require("../../middlewares/authentication");
 const masterRouter = express.Router();
 const masterPembuatRouter = require("./master-pembuat");
+const masterBahanAwalTemplate = require("./master-bahan-awal-template.routes");
 
 // masterRouter.use(authentication);
 masterRouter.use("/pembuat", masterPembuatRouter);
+masterRouter.use("/bahan-awal-template", masterBahanAwalTemplate);
 
 module.exports = masterRouter;
