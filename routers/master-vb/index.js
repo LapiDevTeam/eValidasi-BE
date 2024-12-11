1493bd12582132b7e499736b02f1391bcd04f113
madeariasya <adeariasya@gmail.com>
2024-12-11 12:46:18 +0700

    init master bahan kemas template

--- routers/master-vb/index.js
+++ routers/master-vb/index.js
@@ -4,10 +4,12 @@ const masterRouter = express.Router();
 const masterPembuatRouter = require("./master-pembuat");
 const masterPemasokRouter = require("./master-pemasok");
 const masterItemOri = require("./master-item-ori");
+const masterItemBahanKemasTemplateRouter = require("./master-item-bahan-kemas-template");
 
 // masterRouter.use(authentication);
 masterRouter.use("/pembuat", masterPembuatRouter);
 masterRouter.use("/pemasok", masterPemasokRouter);
 masterRouter.use("/item-ori", masterItemOri);
+masterRouter.use("/bahan-kemas-template", masterItemBahanKemasTemplateRouter);
 
 module.exports = masterRouter;
---
+++ routers/master-vb/master-item-bahan-kemas-template.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const MasterItemBahanKemasTemplateController = require("../../controllers/master-vb/master-item-bahan-kemas-template-controller");
+const masterItemBahanKemasTemplateRouter = express.Router();
+
+module.exports = masterItemBahanKemasTemplateRouter;
