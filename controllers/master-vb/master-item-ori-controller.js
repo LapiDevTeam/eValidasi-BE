beb1d038b6717d33ba8efa08a33b50df23757bc8
madeariasya <adeariasya@gmail.com>
2024-12-12 13:16:02 +0700

    add bpom

--- controllers/master-vb/master-item-ori-controller.js
+++ controllers/master-vb/master-item-ori-controller.js
@@ -1,5 +1,5 @@
-const sql = require("mssql");
-const { configMssql } = require("../../config/configMssql");
+const { sequelizeMSQL } = require("../../config/config.sequelize.dbmssql");
+const { QueryTypes } = require("sequelize");
 const MyError = require("../../helpers/errors");
 const ExcelJS = require("exceljs");
 class MasterItemOriController {
@@ -8,21 +8,18 @@ class MasterItemOriController {
       const { kodeOrNamaBahan = "", isActive, groupType } = req.query;
 
       if (!groupType) throw new MyError(400, "groupType is required");
-      const pool = await sql.connect(configMssql);
       const queryCode = `
       select Item_ID, Group_name, Item_Name, Item_Size, Item_Description, item_unit,
         item_group, item_type, item_Currency, Item_Price, Item_MinOrder, Item_LeadTime, 
         item_PackingSize, Item_Localindent, Item_LastPriceCurrency, item_LastPrice, item_lastPriceDate,
-        item_status, IsActive,Owner, ishalal, item_bpomgenerik, namagenerik, item_row from vwM_ItemWithGroup where item_type = @group_type
+        item_status, IsActive,Owner, ishalal, item_bpomgenerik, namagenerik, item_row from vwM_ItemWithGroup where item_type = '${groupType}'
          ${
            isActive ? "and IsActive = 1" : ""
          } and item_id + ' ' + item_name like '%${kodeOrNamaBahan}%' order by Process_Date desc`;
-      const request = pool.request();
-      const result1 = await request
-        .input("group_type", sql.NVarChar(5), groupType)
-        .query(queryCode);
 
-      const _data = result1.recordset;
+      const _data = await sequelizeMSQL.query(queryCode, {
+        type: QueryTypes.SELECT,
+      });
       res.status(200).json({ data: _data });
     } catch (error) {
       next(error);
@@ -34,19 +31,17 @@ class MasterItemOriController {
       const { itemType } = req.query;
 
       if (!itemType) throw new MyError(400, "Item Type is required");
-      const fileName = "Master Item";
+      const fileName = `Master Item ${itemType}`;
       const workbook = new ExcelJS.Workbook();
       const worksheet = workbook.addWorksheet("Sheet 1");
 
-      const pool = await sql.connect(configMssql);
       const queryCode = `
         Select Type_Name as TIPE, Item_ID as KODE, Group_Name as MASTER , Item_Name as "NAMA BAHAN" , Item_Size as UKURAN , Item_Description  as DESKRIPSI , Item_Unit as SATUAN , Item_MinOrder as "MIN ORDER" , Item_LeadTime as "LEAD TIME" , Item_PackingSize as "PACKING SIZE" , Item_LocalIndent as "LOCAL/INDENT" from vwITEM_PRINT
       where item_type like '${itemType}';
       `;
-      const request = pool.request();
-      const result1 = await request.query(queryCode);
-
-      const dataAudit = result1.recordset;
+      const dataAudit = await sequelizeMSQL.query(queryCode, {
+        type: QueryTypes.SELECT,
+      });
 
       const headers = Object.keys(dataAudit[0] || {});
       const currentDate = new Date().toLocaleDateString("en-GB");
--- routers/master-vb/master-item-bahan-kemas-template.js
+++ routers/master-vb/master-item-bahan-kemas-template.js
@@ -2,4 +2,9 @@ const express = require("express");
 const MasterItemBahanKemasTemplateController = require("../../controllers/master-vb/master-item-bahan-kemas-template-controller");
 const masterItemBahanKemasTemplateRouter = express.Router();
 
+masterItemBahanKemasTemplateRouter.get(
+  "/item-group-template",
+  MasterItemBahanKemasTemplateController.fetchItemWithGroupTemplate
+);
+
 module.exports = masterItemBahanKemasTemplateRouter;
