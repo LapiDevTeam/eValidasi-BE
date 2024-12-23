e5ccdaa6c8753376912b1f9c1e8162139fcd2758
Michael Fransisco <michaelsinx.one@gmail.com>
2024-12-23 07:54:54 +0700

    delete principle

--- routers/master-vb/master-bahan-awal-template.routes.js
+++ routers/master-vb/master-bahan-awal-template.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { masterBahanAwalTemplate_CREATE, masterBahanAwalTemplate_UPDATE, masterBahanAwalTemplate_DELETE, masterBahanAwalTemplate_APPROVE, getViewDPBATemplate, masterItemPrinciple_CREATE, masterItemPrinciple_UPDATE } = require("../../controllers/master-vb/master-bahan-awal-template.controller");
+const { masterBahanAwalTemplate_CREATE, masterBahanAwalTemplate_UPDATE, masterBahanAwalTemplate_DELETE, masterBahanAwalTemplate_APPROVE, getViewDPBATemplate, masterItemPrinciple_CREATE, masterItemPrinciple_UPDATE, masterItemPrinciple_DELETE } = require("../../controllers/master-vb/master-bahan-awal-template.controller");
 const router = express.Router();
 
 router.post(
@@ -29,6 +29,7 @@ router.delete(
 
 router.post('/principle', masterItemPrinciple_CREATE);
 router.patch('/principle', masterItemPrinciple_UPDATE);
+router.delete('/principle', masterItemPrinciple_DELETE);
 
 
 module.exports = router;
