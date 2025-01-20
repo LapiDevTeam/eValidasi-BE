d8fcd0583625e2624d831a228195308702cb8391
Michael Fransisco <michaelsinx.one@gmail.com>
2025-01-20 08:46:00 +0700

    update master ppi getter

--- controllers/master-vb/master-formula.controller.js
+++ controllers/master-vb/master-formula.controller.js
@@ -218,6 +218,7 @@ const getPPI = async (req, res) => {
 
     let strSQL = `
       SELECT
+        PPI_ID + PPI_SUBID + PPI_PRODUCTID + CONVERT(VARCHAR(1), PPI_PRODUCTINIT) as tag,
         PPI_ID,
         PPI_ProductID,
         PPI_ProductInit,
