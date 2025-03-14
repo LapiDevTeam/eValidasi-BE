f093bac8c4d82aebaee0a2a5f39b0dc39df974cc
Michael Fransisco <michaelsinx.one@gmail.com>
2025-03-14 08:34:27 +0700

    continue dev KOREKSI GUDANG RD

--- routers/master-vb/koreksi-stockRD.routes.js
+++ routers/master-vb/koreksi-stockRD.routes.js
@@ -2,10 +2,17 @@ const express = require('express');
 const trxKoreksiStockRDRouter = express.Router();
 
 const { authentication } = require('../../middlewares/authentication');
-const { sbCekButton } = require('../../controllers/master-vb/trx-koreksi-stockRD.controller');
+const { sbCekButton, cmdFindData1, cmdFindData2, cmdDelete, cmdApprove, getDetailGrid, cmdSave } = require('../../controllers/master-vb/trx-koreksi-stockRD.controller');
 
 
 trxKoreksiStockRDRouter.get('/cekbuttons', authentication, sbCekButton);
+trxKoreksiStockRDRouter.get('/getdocuments', authentication, cmdFindData1);
+trxKoreksiStockRDRouter.get('/getdetailgrid', authentication, getDetailGrid);
+trxKoreksiStockRDRouter.get('/getkodebahan', authentication, cmdFindData2);
+
+trxKoreksiStockRDRouter.delete('/deletekoreksi', authentication, cmdDelete);
+trxKoreksiStockRDRouter.post('/approve', authentication, cmdApprove);
+trxKoreksiStockRDRouter.post('/save', authentication, cmdSave);
 
 
 
