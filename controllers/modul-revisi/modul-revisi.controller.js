5c091c3d8cbe0b0ffbaaca20986facebccc188ed
Michael Fransisco <michaelsinx.one@gmail.com>
2025-06-12 07:48:12 +0700

    update module revision

--- controllers/modul-revisi/modul-revisi.controller.js
+++ controllers/modul-revisi/modul-revisi.controller.js
@@ -26,7 +26,8 @@ const getModuleRevisionsDA = async (req, res) => {
         appr_userid,
         appr_delegated,
         appr_date,
-        extraData
+        extraData,
+        mgr_userid
       FROM m_module_revisions
       WHERE modulename = :modulename
     `;
@@ -75,7 +76,7 @@ const getModuleRevisionsDA = async (req, res) => {
 
 const createModuleRevision = async (req, res) => {
   try {
-    const { modulename, tgl_revisi, alasan_desc, daftar_distribusi = null, refrensi = null, dokumen_terkait = null, appr_userid, appr_delegated, appr_date, extraData } = req.body;
+    const { modulename, tgl_revisi, alasan_desc, daftar_distribusi = null, refrensi = null, dokumen_terkait = null, appr_userid, appr_delegated, appr_date, extraData, mgr_userid } = req.body;
 
     if (!modulename || !tgl_revisi || !alasan_desc) {
       return res.status(400).json({ message: 'modulename, tgl_revisi, and alasan_desc are required.' });
@@ -121,7 +122,8 @@ const createModuleRevision = async (req, res) => {
         appr_userid,
         appr_delegated,
         appr_date,
-        extraData
+        extraData,
+        mgr_userid
       )
       VALUES (
         :PK_ID,
@@ -136,7 +138,8 @@ const createModuleRevision = async (req, res) => {
         :appr_userid,
         :appr_delegated,
         :appr_date,
-        :extraData
+        :extraData,
+        :mgr_userid
       )
     `;
 
@@ -154,6 +157,7 @@ const createModuleRevision = async (req, res) => {
         appr_delegated: appr_delegated || '',
         appr_date: appr_date || null,
         extraData: extraData || null,
+        mgr_userid: mgr_userid || null,
       },
       type: Sequelize.QueryTypes.INSERT,
     });
@@ -167,7 +171,7 @@ const createModuleRevision = async (req, res) => {
 
 const createModuleRevisionWithSameNumber = async (req, res) => {
   try {
-    const { modulename, tgl_revisi, alasan_desc, daftar_distribusi = null, refrensi = null, dokumen_terkait = null, appr_userid, appr_delegated, appr_date, extraData } = req.body;
+    const { modulename, tgl_revisi, alasan_desc, daftar_distribusi = null, refrensi = null, dokumen_terkait = null, appr_userid, appr_delegated, appr_date, extraData, mgr_userid } = req.body;
 
     if (!modulename || !tgl_revisi || !alasan_desc) {
       return res.status(400).json({ message: 'modulename, tgl_revisi, and alasan_desc are required.' });
@@ -217,7 +221,8 @@ const createModuleRevisionWithSameNumber = async (req, res) => {
         appr_userid,
         appr_delegated,
         appr_date,
-        extraData
+        extraData,
+        mgr_userid
       )
       VALUES (
         :PK_ID,
@@ -232,7 +237,8 @@ const createModuleRevisionWithSameNumber = async (req, res) => {
         :appr_userid,
         :appr_delegated,
         :appr_date,
-        :extraData
+        :extraData,
+        :mgr_userid
       )
     `;
 
@@ -250,6 +256,7 @@ const createModuleRevisionWithSameNumber = async (req, res) => {
         appr_delegated: appr_delegated || '',
         appr_date: appr_date || null,
         extraData: extraData || null,
+        mgr_userid: mgr_userid || null,
       },
       type: Sequelize.QueryTypes.INSERT,
     });
@@ -327,60 +334,8 @@ const getLatestModuleRevisionNumber = async (req, res) => {
   }
 };
 
-const approveModuleRevisionByModuleName = async (modulename, user_id, delegated_to) => {
-  const transaction = await sequelizeMSQL.transaction();
-  try {
-    if (!modulename || !user_id || !delegated_to) {
-      throw new Error('modulename, user_id, and delegated_to are required.');
-    }
-
-    const sqlDtTime = moment().format('YYYY-MM-DD HH:mm:ss');
-
-    // Check if the user is an approver (change 'MODULE' if your application code is different)
-    const approver = await sequelizeMSQL.query(
-      `SELECT TOP 1 Appr_Identity FROM m_Approver_Lines WHERE isactive = 1 AND Appr_ApplicationCode LIKE 'MODULE' AND Appr_ID LIKE :user_id`,
-      { replacements: { user_id }, type: QueryTypes.SELECT }
-    );
-
-    if (!approver || approver.length === 0) {
-      throw new Error('User is not authorized to approve.');
-    }
-
-    const sqlAppr_Identity = approver[0]?.Appr_Identity || '0000';
-
-    if (!sqlAppr_Identity || sqlAppr_Identity === '0000') return 0; // Approval failed
-    console.log({ Approval: sqlAppr_Identity, status: "failed" });
-
-    const updateQuery = `
-      UPDATE m_module_revisions
-      SET appr_userid = :user_id,
-          appr_delegated = :delegated_to,
-          appr_date = :sqlDtTime
-      WHERE modulename = :modulename
-        AND appr_date IS NULL;
-    `;
-
-    const [updateResult] = await sequelizeMSQL.query(updateQuery, {
-      replacements: { user_id, delegated_to, sqlDtTime, modulename },
-      transaction,
-    });
-
-    // Commit the transaction if the update was successful
-    if (updateResult > 0) {
-      await transaction.commit();
-      return 1; // Approval successful
-    } else {
-      throw new Error('No revisions found to approve.');
-    }
-  } catch (error) {
-    console.error('Error approving module revision:', error);
-    await transaction.rollback();
-    return 0; // Approval failed
-  }
-};
-
 const updateOrCreateModuleRevision = async (req, res) => {
-  const { modulename, no_revisi, tgl_revisi, alasan_desc, daftar_distribusi = null, refrensi = null, dokumen_terkait = null, appr_userid, appr_delegated, appr_date, extraData } = req.body;
+  const { modulename, no_revisi, tgl_revisi, alasan_desc, daftar_distribusi = null, refrensi = null, dokumen_terkait = null, appr_userid, appr_delegated, appr_date, extraData, mgr_userid } = req.body;
 
   if (!modulename || !no_revisi || !tgl_revisi || !alasan_desc) {
     return res.status(400).json({ message: "All fields are required." });
@@ -423,7 +378,8 @@ const updateOrCreateModuleRevision = async (req, res) => {
             appr_userid = :appr_userid,
             appr_delegated = :appr_delegated,
             appr_date = :appr_date,
-            extraData = :extraData
+            extraData = :extraData,
+            mgr_userid = :mgr_userid
         WHERE no_revisi = :no_revisi AND modulename = :modulename
       `;
 
@@ -440,6 +396,7 @@ const updateOrCreateModuleRevision = async (req, res) => {
           appr_delegated: appr_delegated || '',
           appr_date: appr_date || null,
           extraData: extraData || null,
+          mgr_userid: mgr_userid || null,
         },
         transaction,
       });
@@ -469,7 +426,8 @@ const updateOrCreateModuleRevision = async (req, res) => {
           appr_userid,
           appr_delegated,
           appr_date,
-          extraData
+          extraData,
+          mgr_userid
         )
         VALUES (
           :PK_ID,
@@ -484,7 +442,8 @@ const updateOrCreateModuleRevision = async (req, res) => {
           :appr_userid,
           :appr_delegated,
           :appr_date,
-          :extraData
+          :extraData,
+          :mgr_userid
         )
       `;
 
@@ -502,6 +461,7 @@ const updateOrCreateModuleRevision = async (req, res) => {
           appr_delegated: appr_delegated || '',
           appr_date: appr_date || null,
           extraData: extraData || null,
+          mgr_userid: mgr_userid || null,
         },
         transaction,
       });
