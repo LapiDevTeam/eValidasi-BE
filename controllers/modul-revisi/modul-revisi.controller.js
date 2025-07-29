a6a01ddd3e9ebf4b1bc9d55acb1e574a1445adf6
Michael Fransisco <michaelsinx.one@gmail.com>
2025-07-29 15:54:45 +0700

    update asp link

--- controllers/modul-revisi/modul-revisi.controller.js
+++ controllers/modul-revisi/modul-revisi.controller.js
@@ -569,7 +569,7 @@ const getAspLink = async (req, res) => {
     }
 
     // Construct the ASP link
-    const aspLink = `http://192.168.1.40:8080/${menuName}/AutoLogin.aspx?UID=${user_id}&DID=${user_id}&Token=${token}&page=${pageName}`;
+    const aspLink = `http://192.168.1.39:8080/${menuName}/AutoLogin.aspx?UID=${user_id}&DID=${user_id}&Token=${token}&page=${pageName}`;
 
     return res.status(200).json({ message: 'Success.', data: aspLink });
   } catch (error) {
