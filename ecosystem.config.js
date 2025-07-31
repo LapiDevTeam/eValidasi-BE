a3a4df2e19d732a41b45daf70267e88d2fbf4fae
esyaahmad <esya.ahmad@gmail.com>
2025-07-31 15:08:30 +0700

    auth

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 module.exports = {
   apps: [
     {
-      name: "eFormulation-dev",
+      name: "ePengembanganFormula",
       script: "./index.js",
       env: {
         NODE_ENV: "development",
