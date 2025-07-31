8d3dfa9590725335132db4770f276143a140ca91
Michael Fransisco <michaelsinx.one@gmail.com>
2025-07-31 11:56:49 +0700

    update middlewares

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -9,7 +9,7 @@ const authentication = async (req, res, next) => {
     if (!authentication) throw new MyError(401, "Not Authentication");
     if (authentication) {
       const response = await fetch(
-        "http://192.168.1.38/api/lms-dev/v1/decode",
+        "http://192.168.1.38/api/lms/v1/decode",
         {
           method: "GET",
           headers: {
