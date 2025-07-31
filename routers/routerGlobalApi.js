1a3489ac84aacb5e564451513c90b7b559ad486e
Michael Fransisco <michaelsinx.one@gmail.com>
2025-07-31 17:19:48 +0700

    test auth

--- routers/routerGlobalApi.js
+++ routers/routerGlobalApi.js
@@ -27,6 +27,6 @@ router.post('/module-revisions/approve', async (req, res) => {
 });
 
 router.get('/asp-link', authentication, modulRevisiController.getAspLink);
-router.get('/printDA', authentication, modulRevisiController.printHeaderDa);
+router.get('/printDA', modulRevisiController.printHeaderDa);
 
 module.exports = router;
