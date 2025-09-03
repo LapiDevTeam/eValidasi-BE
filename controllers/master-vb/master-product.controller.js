168f69cf09a0991c05c1d5480e353b2125c98efd
Michael Fransisco <michaelsinx.one@gmail.com>
2025-09-03 16:35:20 +0700

    insert jenis kemasan ke m_product_template saat approve

--- controllers/master-vb/master-product.controller.js
+++ controllers/master-vb/master-product.controller.js
@@ -774,7 +774,7 @@ class MasterProductController {
     `;
       console.log({ sSQL5 });
       let sSQL6 = `
-      INSERT INTO m_Product_template (Kategori_prod, Product_ID, Product_Init, Product_Name, jenis_prod, Product_Category, Product_Currency, Product_HPP, Product_HNA, Product_HTollin, Product_HTollInFee,
+      INSERT INTO m_Product_template (jenis_kemasan, Kategori_prod, Product_ID, Product_Init, Product_Name, jenis_prod, Product_Category, Product_Currency, Product_HPP, Product_HNA, Product_HTollin, Product_HTollInFee,
         Product_VolumeInBox, Product_VolumeInBigBox, Product_Unit, Product_Type, Product_IntermediateID, isActive, User_ID, Delegated_To, Process_Date,
         Product_Sediaan, Product_ExpTime, Product_SalesID, Product_isPPA, Product_isPseudo, Product_isBuy, Product_Batchsize, Product_Location,
         Product_GroupSediaan, Product_GroupTargetRealisasi, Product_SalesName, Product_Status, Product_SalesHNA, Product_ShortName, Product_SalesUnit,
@@ -782,7 +782,7 @@ class MasterProductController {
         Product_RetainSmallUnit, Product_SediaanPlanning, Product_Owner, product_import, Product_KonversiProduksiToSales, Product_bahanAktif,
         Product_PN_HK_PProcessing, Product_PN_HK_PKSekunder, Product_PN_HK_Produksi, Product_PN_HK_PPIProduksi, Product_versiBPOM, Product_NotPPI,
         Product_BentukSediaan, Product_Kemasan, Product_Dosis, Product_RuangLingkup, PK_ID, Product_Periode, CDOB_01, CDOB_02, CDOB_03, Approve_date, User_Approve, User_Delegated)
-      SELECT Kategori_prod, Product_ID, Product_Init, Product_Name, jenis_prod, Product_Category, Product_Currency, Product_HPP, Product_HNA, Product_HTollin, Product_HTollInFee,
+      SELECT jenis_kemasan, Kategori_prod, Product_ID, Product_Init, Product_Name, jenis_prod, Product_Category, Product_Currency, Product_HPP, Product_HNA, Product_HTollin, Product_HTollInFee,
         Product_VolumeInBox, Product_VolumeInBigBox, Product_Unit, Product_Type, Product_IntermediateID, isActive, User_ID, Delegated_To, Process_Date,
         Product_Sediaan, Product_ExpTime, Product_SalesID, Product_isPPA, Product_isPseudo, Product_isBuy, Product_Batchsize, Product_Location,
         Product_GroupSediaan, Product_GroupTargetRealisasi, Product_SalesName, Product_Status, Product_SalesHNA, Product_ShortName, Product_SalesUnit,
